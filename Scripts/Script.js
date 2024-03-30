ef2cd0c8080b811ee96bedbe7081f66eebfe59ae
Giovanni Peirone <mgpeirone@escuelasproa.edu.ar>
2024-03-30 16:32:34 -0300

    bmn

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,15 +1,18 @@
 
 function polindromDetecter() {
+  const Result = document.getElementById("RES");
+  Result.innerText = "";
   let name = document.getElementById("input-name").value;
   let Array_name = [];
   Array_name = name.split("").reverse().join("");
 
   if (Array_name == name) {
-    document.getElementById('')
+    Result.innerText = "The name is a polindrom";
     //alert("The name is a polindrom");
-  }else {
     
-    //alert("Tne name is not a polindrom");
+  }else {
+    Result.innerText = "The name is not a polindrom";
+    //alert("The name is a polindrom");
   }
 }
 
